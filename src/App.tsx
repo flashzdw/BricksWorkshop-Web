40bcedb7b6f35f524f445576a353a32aac8dd618
flashzdw <flashzdw@users.noreply.github.com>
2026-04-04 07:58:03 +0000

    feat: 制作BricksWorkshop宣传网站
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/BricksWorkshop-Web">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
